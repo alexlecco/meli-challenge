562d8c8836e557a3c42abcf2fbe966862aa71050
alexlecco <alexlecco24@gmail.com>
2021-01-25 16:46:01 -0300

    improve product design

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,18 +14,30 @@ const IndexPage: React.FC<Props> = ({ results }) => {
   return (
     <Box centerContent padding={4}>
       <Stack width="100%" backgroundColor="white" borderRadius={2} boxShadow="sm" padding={4}>
-        {results.map(product => <Stack key={product.id} direction="row">
-          <Box
-            backgroundColor="gray.50"
-            backgroundImage={`url(${product.image})`}
-            height={180}
-            width={180}
-          />
-          <Stack>
-            <Text>${product.price}</Text>
-            <Text>{product.title}</Text>
-          </Stack>
-        </Stack>)}
+        {
+          results.map(product =>
+            <Stack key={product.id} direction="row" justifyContent="space-between">
+              <Stack direction="row">
+                <Box
+                  backgroundColor="gray.50"
+                  backgroundImage={`url(${product.image})`}
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                  backgroundSize="contain"
+                  borderRadius="sm"
+                  height={180}
+                  width={180}
+                />
+                <Stack>
+                  <Text fontSize={{ base: "2xl", lg: "lg", xl: "sm" }} fontWeight={500}>
+                    {product.price.toLocaleString("es-Ar", { style: "currency", currency: "EUR" })}
+                  </Text>
+                  <Text>{product.title}</Text>
+                </Stack>
+              </Stack>
+              <Text fontSize="sm">{product.location}</Text>
+            </Stack>)
+        }
       </Stack>
     </Box>
   )
