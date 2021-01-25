885efc5f2f51d4c586f7fc4172ee176870ccf86d
alexlecco <alexlecco24@gmail.com>
2021-01-25 16:24:06 -0300

    render search results

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,44 @@
-import React from 'react'
-import { GetServerSideProps } from 'next'
+import React from 'react';
+import { GetServerSideProps } from 'next';
 import api from '../product/api';
-import { Product } from '../product/types'
+import { Product } from '../product/types';
+import { Container, Box, Stack, Text } from '@chakra-ui/react';
 
 interface Props {
-  results: Product[]
+  results: Product[];
 }
 
 const IndexPage: React.FC<Props> = ({ results }) => {
-  console.log({ results })
+  console.log({ results });
 
-  return <div> Hello Twitch </div>
-}
+  return (
+    <Box centerContent padding={4}>
+      <Stack width="100%" backgroundColor="white" borderRadius={2} boxShadow="sm" padding={4}>
+        {results.map(product => <Stack key={product.id} direction="row">
+          <Box
+            backgroundColor="gray.50"
+            backgroundImage={`url(${product.image})`}
+            height={180}
+            width={180}
+          />
+          <Stack>
+            <Text>${product.price}</Text>
+            <Text>{product.title}</Text>
+          </Stack>
+        </Stack>)}
+      </Stack>
+    </Box>
+  )
+};
 
 export const getServerSideProps: GetServerSideProps = async ({ query }) => {
-  const results = await api.search(query.q as string);
+  const results = await api.search((query.q as string));
 
   return {
     props: {
-      results,
-    },
+      results
+    }
   };
 };
 
-export default IndexPage
+export default IndexPage;
